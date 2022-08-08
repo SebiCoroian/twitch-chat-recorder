88afd7d3567af90a0d25067d04caa1400b42469c
Sebi Coroian <escapemcro@gmail.com>
2022-08-08 03:26:04 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import config
+import socket
+import json
+import time
+import re
+
+#socket creation
+monitor = socket.socket()
+monitor.connect((config.server,config.port))
+
+def serialize (chatlog):
+	regex=re.search(r":([a-zA-Z0-9_]+).+:(.*)\r", chatlog)
+	timestamp = time.time()
+	user = str(regex.group(1))
+	message = str(regex.group(2))
+	data = {
+    "chat": {
+        "username": user,
+        "time": timestamp,
+        "message": message
+    	}
+	}
+	serialized = json.dumps(data,indent=2)
+	with open("chatlog.json", "a") as outfile:
+		outfile.write(serialized)
+	
+
+def init():
+	#twitch connection
+	monitor.send(f"PASS {config.token}\n".encode('utf-8'))
+	monitor.send(f"NICK {config.nickname}\n".encode('utf-8'))
+	monitor.send(f"JOIN {config.channel}\n".encode('utf-8'))
+	loop()
+
+def loop():
+	while(1==1):
+		payload = str(monitor.recv(2048).decode('utf-8'))
+		serialize(payload)
+
+init()
